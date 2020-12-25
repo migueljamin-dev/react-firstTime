eb88500688319c9c80e0861c078cfbc38157b59f
Miguel Jamin <migueljamin12@gmail.com>
2020-12-25 21:37:01 +0800

    Config React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import  {Navbar , NavbarBrand} from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar dark color="primary">
+       <div className="container">
+         <NavbarBrand href ="/">Hello</NavbarBrand>
+       </div>
+     </Navbar>
+    </div>
+  );
+}
+
+export default App;
